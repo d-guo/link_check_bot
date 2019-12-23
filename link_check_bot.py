6a3a4b99bb71a534385fdce268bddcc19631f936
dguo <dguo@ucsb.edu>
2019-12-23 06:29:31 -0800

    bot is able to connect and retrieve info from subreddits

--- link_check_bot.py
+++ link_check_bot.py
@@ -1,2 +1,9 @@
 import praw
 
+# 'link_check_bot' is taken from praw.ini
+reddit = praw.Reddit('link_check_bot')
+
+# looking at this particular subreddit just for testing purposes
+subreddit = reddit.subreddit('cubing')
+
+print(subreddit.description)
