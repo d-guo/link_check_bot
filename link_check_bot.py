b40f86c7b89b728fa199eb335df572bfcd2262d7
dguo <dguo@ucsb.edu>
2019-12-27 04:01:57 -0800

    able to find which posts have a link in them

--- link_check_bot.py
+++ link_check_bot.py
@@ -1,11 +1,20 @@
 import praw
 
+# common image extensions
+image_extensions = ('.png', '.jpeg', '.jpg', 'gif')
+
 # 'link_check_bot' is taken from praw.ini
 reddit = praw.Reddit('link_check_bot')
 
 # looking at this particular subreddit just for testing purposes
-subreddit = reddit.subreddit('cubing')
+subreddit = reddit.subreddit('danielguo')
 
-print(subreddit.title)
-print(subreddit.name)
-print(subreddit.description)
+# check all recent and incoming submissions for link
+for submission in subreddit.stream.submissions():
+    #if this if statement is triggered, then submission is either a link or an image
+    if(not submission.is_self):
+        content = submission.url
+        #if this is statement is triggered, then submission is not an image and we want to check its safety
+        if(not content.endswith(image_extensions)):
+            print(content)
+
