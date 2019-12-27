c3ab5bd56d3be2efb27a05201988d4271b9cb0f3
dguo <dguo@ucsb.edu>
2019-12-27 04:34:22 -0800

    hey it works lol

--- link_check_bot.py
+++ link_check_bot.py
@@ -1,20 +1,32 @@
 import praw
+from urllib.request import urlopen
 
 # common image extensions
 image_extensions = ('.png', '.jpeg', '.jpg', 'gif')
 
-# 'link_check_bot' is taken from praw.ini
-reddit = praw.Reddit('link_check_bot')
+# returns true if url exists
+def url_exists(url):
+    return urlopen(url).code < 400
 
-# looking at this particular subreddit just for testing purposes
-subreddit = reddit.subreddit('danielguo')
+def main():
+    # 'link_check_bot' is taken from praw.ini
+    reddit = praw.Reddit('link_check_bot')
 
-# check all recent and incoming submissions for link
-for submission in subreddit.stream.submissions():
-    #if this if statement is triggered, then submission is either a link or an image
-    if(not submission.is_self):
-        content = submission.url
-        #if this is statement is triggered, then submission is not an image and we want to check its safety
-        if(not content.endswith(image_extensions)):
-            print(content)
+    # looking at this particular subreddit just for testing purposes
+    subreddit = reddit.subreddit('danielguo')
 
+    # check all recent and incoming submissions for link
+    for submission in subreddit.stream.submissions():
+        #if this if statement is triggered, then submission is either a link or an image
+        if(not submission.is_self):
+            url = submission.url
+            #if this is statement is triggered, then submission is not an image and we want to check it
+            if(not url.endswith(image_extensions)):
+                if(url_exists(url)):
+                    message = f"I have checked the url {url}, and it DOES exist."
+                else:
+                    message = f"I have checked the url {url}, and it DOES NOT exist."
+                submission.reply(message)
+
+if(__name__ == "__main__"):
+    main()
